e070b211e80b42c0435af8f790592473e6c498a9
Ashton Wray <aricks1986@gmail.com>
2025-03-16 12:17:31 -0400

    made changes to NotesViewModel, AllNotesPage, and added .gitignore file

--- ViewModels/NotesViewModel.cs
+++ ViewModels/NotesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Notes.Models;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -28,5 +29,36 @@ internal class NotesViewModel : IQueryAttributable
     private async Task SelectNoteAsync(ViewModels.NoteViewModel note)
     {
         await Shell.Current.GoToAsync("${nameof(Views.NotePage)}?load={note.Identifier}");
+    }
+
+    // Method to implement the IQueryAttributable interface
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("deleted"))
+        {
+            string noteId = query["deleted"].ToString();
+            NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note exists, delete it
+            if (matchedNote != null)
+            {
+                AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                NoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                    matchedNote.Reload();
+
+                // If note isn't found, it's new; add it
+                else
+                {
+                    AllNotes.Add(new NoteViewModel(Note.Load(noteId)));
+                }
+            }
+        }
     }
 }
--- obj/Debug/net8.0-android/Notes.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/Notes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cb4e45711337a881cbfbffa1bfc47df86a7aac23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
